80c54b90d09af4badd170b9860df8b1090d77e48
caitlin <caitlinong@gmail.com>
2020-03-17 08:42:10 +0800

    New commit pie.js

---
+++ grocerychart/src/pie.js
@@ -0,0 +1,41 @@
+import {Pie} from 'vue-chartjs'
+export default{
+    extends:Pie,
+    data: () => ({
+        chartdata: {
+          labels: ['January', 'February','March','April'],
+          datasets: [
+            {
+              label: 'Sales',
+              data: [400, 300,150,100],
+              backgroundColor:['deepskyblue','springgreen','lightcoral','gold'],
+              borderWidth:0.5,
+              borderColor:"#000"
+            }
+          ]
+          
+        },
+        options: {
+            scales:{
+                yAxes:[{
+                    ticks:{
+                        min:0
+                    }
+
+                }]
+            },
+            layout:{
+              padding:{
+                  left: 5,
+                  right: 0,
+                  top: 0,
+                  bottom: 10
+              }
+          }
+          
+        }
+      }),
+    mounted(){
+        this.renderChart(this.chartdata,this.options)
+    }
+}
